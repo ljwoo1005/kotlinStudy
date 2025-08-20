9fb1661bbf9baa9691303c70574d3300ca983293
ljwoo1005 <ljwoo1005@naver.com>
2025-08-20 22:45:14 +0900

    2025.08.20 부족한 정보 보충

--- Scratches/04-kotiln grammar/06.interface.kts
+++ Scratches/04-kotiln grammar/06.interface.kts
@@ -84,6 +84,4 @@ interface Inter2 {
 }
 
 // 컴파일 에러 : Cannot infer visibility for 'method'. Specify it explicitly
-class InterImpl : Inter1, Inter2 {
-
-}
+class InterImpl : Inter1, Inter2 { }
--- Scratches/04-kotiln grammar/test.kts
+++ Scratches/04-kotiln grammar/test.kts
@@ -1,22 +1,12 @@
-class Person3 {
-    init {
+val citySet : Set<String> = setOf("서울", "수원", "부산")
 
-    }
+val citySet2 = mutableSetOf("서울", "수원", "부산")
+citySet2.add("안양")  // [서울, 수원, 부산, 안양]
+citySet2.remove("수원") // [서울, 부산, 안양]
 
-    constructor(name : String) {
-        println(name)
-    }
+println(citySet2.size) // 3
 
-}
+println(citySet2.contains("서울")) // true
 
 
-class Person4(var name : String) {
-    init {
-        println(name)
-    }
-
-    fun nameCall() {
-        println(name)
-    }
-}
 
