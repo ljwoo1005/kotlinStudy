c3cfefd47aa22ff260dc7a0101f4d0f49a8d2c05
LEE JIN WOO <ljw1005@smartcitykorea.com>
2025-09-16 16:55:45 +0900

    2025.09.16 RelativeLayout

---
+++ ch07/src/main/java/com/example/ch07/relativelayout01/RelativeLayout01.kt
@@ -0,0 +1,21 @@
+package com.example.ch07.relativelayout01
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ch07.R
+
+class RelativeLayout01 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_relative_layout01)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
