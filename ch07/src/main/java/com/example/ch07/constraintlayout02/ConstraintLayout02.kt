766fe797b957e16330f83090694f7cd645b43ce4
LEE JIN WOO <ljw1005@smartcitykorea.com>
2025-09-17 17:27:31 +0900

    2025.09.17 ConstraintLayout 추가

---
+++ ch07/src/main/java/com/example/ch07/constraintlayout02/ConstraintLayout02.kt
@@ -0,0 +1,21 @@
+package com.example.ch07.constraintlayout02
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ch07.R
+
+class ConstraintLayout02 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_constraint_layout02)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
